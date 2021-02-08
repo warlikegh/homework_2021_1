9bce858bc7d0e0cd546486a0e4fb4f6bb2bcac42
warlikegh <a_katnov@mail.ru>
2021-02-08 11:57:38 +0300

    add jsDoc

--- source/inverse.js
+++ source/inverse.js
@@ -1,10 +1,24 @@
 'use strict';
 
+/**
+ * The "inverse" function reverses the order of the elements in an array.
+ * If a number is passed to the function as the second argument,
+ * then all the elements of the array are rearranged except for the first few
+ * (the number depends on the number).
+ * If the number is negative, then the elements at the end of the array remain in place
+ *
+ * @param {Array} array - An array of elements (won`t change)
+ * @param {number} stat - A number of elements to skip. If the number is negative,
+ * then the elements at the end of the array remain in place.
+ * @return {Array} inversed array
+ *
+ * @author Katnov Artem
+ */
 const inverse = (array, stat = 0) => {
     if (!(Array.isArray(array) && Number.isInteger(stat))) {
         throw new Error("Please, try with correct data: Array (and maybe Number)");
     }
-    let [array1, array2] = [array.slice(0, stat), array.slice(stat, array.length)];
+    const [array1, array2] = [array.slice(0, stat), array.slice(stat, array.length)];
 
     return stat >= 0 ? array1.concat(array2.reverse()) : array1.reverse().concat(array2);
 };
