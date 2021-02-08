3084884a035b418b730e2d6b79d3063bd28aa2ef
warlikegh <a_katnov@mail.ru>
2021-02-09 00:08:55 +0300

    correct style

--- source/inverse.js
+++ source/inverse.js
@@ -18,7 +18,8 @@ const inverse = (array, stat = 0) => {
     if (!(Array.isArray(array) && Number.isInteger(stat))) {
         throw new Error("Please, try with correct data: Array (and maybe Number)");
     }
-    const [array1, array2] = [array.slice(0, stat), array.slice(stat, array.length)];
+    const array1 = array.slice(0, stat);
+    const array2 = array.slice(stat, array.length);
 
     return stat >= 0 ? array1.concat(array2.reverse()) : array1.reverse().concat(array2);
 };
